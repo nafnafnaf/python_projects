39836b13ad8fe007a9da8bdf74316d75a04f1238
dimi nafpigos <dimi@dimi-ThinkPad-X61s.lan>
2017-10-01 03:21:50 +0300

    first version TelegramMeteo bot

---
+++ final3.py
@@ -0,0 +1,33 @@
+import sys, time, telepot, unicodedata, urllib3, wikiquote, random
+from telepot.loop import MessageLoop
+from urllib.request import urlopen, Request #as uReq
+from bs4 import BeautifulSoup as soup
+#BeautifulSoup + Wikiquote part
+def scrap():
+    url = 'http://www.meteokav.gr/weather/'
+    req = urlopen(url)
+    page = req.read()
+    req.close()
+    page_soup = soup(page, "html.parser")
+    containers = page_soup.find("span", {"id":"ajaxtemp"}).text.strip()
+    norm_containers = unicodedata.normalize('NFKD', containers).encode('ascii', 'ignore')+ b"\n"
+    the_quote = random.choice(wikiquote.quotes('Helen Keller')).encode('UTF-8') 
+    output = norm_containers +  the_quote
+    return output
+#Telepot part    
+def handle(msg):
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    print(content_type, chat_type, chat_id)
+
+    if content_type == 'text':
+        bot.sendMessage(chat_id, scrap())
+
+TOKEN = '429145952:AAFTuqbZQlnsS6TbmtKvbWwxYbxDp59GkLw'
+
+bot = telepot.Bot(TOKEN)
+MessageLoop(bot, handle).run_as_thread()
+print ('Listening ...')
+
+# Keep the program running.
+while 1:
+    time.sleep(1)
